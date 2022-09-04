cf79d2ec816622b6ef35121e7b616bb7971f9094
Maxun <trukhinmaksimof@mail.ru>
2022-09-05 02:26:45 +0800

    created coins grid

--- index.js
+++ index.js
@@ -3,15 +3,37 @@ import {countMoneySum, changeCashRegister} from "./change_money.js";
 let coins = {};
 
 $(document).ready(() => {
-    coins = {"PENNY" : .01, "NICEL" : .05 ,"DIME" : .1, "QUARTER" : .25, "ONE" : 1, "FIVE" : 5,"TEN" : 10,"TWENTY" : 20, "ONE HUNDRED" : 100};
+    coins = {"PENNY" : .01, "NICEL" : .05 ,"DIME" : .1, "QUARTER" : .25, "ONE" : 1, "FIVE" : 5,"TEN" : 10,"TWENTY" : 20, "HUNDRED" : 100};
     
     let selectedItem = null;
+    let coinsStackHeight = 0;
+    let sumToPay = 0;
+
+    function stackCoins(coinKey) {
+        return function () {
+            let droppedCoin = $(this).clone();
+
+            console.log("stack");
+            user.coins[coinKey] -= 1;
+            console.log($($(this).children()[0]).text());
+            sumToPay += coins[coinKey];
+            $(droppedCoin).css({
+                "position" : "absolution",
+                "top" : "50px",
+                "left" : "500px"
+            });
+
+            if (!user.coins[coinKey]) this.style.opacity = "0.6";
+        }
+    }
+
     const user = {  
         init : function(coinsReference) {
             this.money = 100;
             this.coins = {...coinsReference}; // Object with coin's names : number of these coin
             Object.keys(this.coins).map(coinKey => this.coins[coinKey] = 0);
             this.coins[Object.keys(this.coins).at(-1)] = 1; // add a biggest coin to the user's pocket
+            
             this.items = {};
             console.table(this.coins);
         },
@@ -41,8 +63,21 @@ $(document).ready(() => {
         selectCoinsToPay : function(item) {
             if (!item) return undefined;
             let cash = {};
+            let htmlCoins = [];
+            let newCoinItem = '';
             
             $(".payment-modal").css("display", "flex");
+            for (let coin in coins) {
+                if (this.coins[coin]) {
+                    newCoinItem = "<div class='coin-wrapper'><div class='coin'>" + coin + "<br>" + coins[coin] + "$</div>" + this.coins[coin] + "</div>";
+                    $(".payment-modal > .grid").append(newCoinItem);    
+                    $(".payment-modal > .grid").click(stackCoins(coin));
+                } else {
+                    newCoinItem = "<div class='coin-wrapper'><div class='coin' style='opacity: 0.6;'>" + coin + "<br>" + coins[coin] + "$</div>" + this.coins[coin] + "</div>";
+                    $(".payment-modal > .grid").append(newCoinItem);
+                }
+                
+            }
         }
     };
     
